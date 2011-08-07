3e554e05ed6b04353d5dafe00e200d60160ec01a
Dmtiry Shatrov <shatrov@gmail.com>
2011-08-08 00:32:51 +0400

    Added extra headers for inet_aton (according to inet_aton(3) man page in FreeBSD).

--- libmary/util_net.h
+++ libmary/util_net.h
@@ -24,7 +24,10 @@
 #include <libmary/types.h>
 
 #ifndef PLATFORM_WIN32
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #else
 #include <Winsock2.h>
 #endif
