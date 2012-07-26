701b7f046045d1ec92d832c182eb628c889eb0f1
Dmitry Shatrov <shatrov@gmail.com>
2012-07-26 15:44:46 +0400

    md5 support

--- libmary/libmary.h
+++ libmary/libmary.h
@@ -117,6 +117,7 @@
 #include <libmary/util_time.h>
 #include <libmary/util_net.h>
 #include <libmary/util_dev.h>
+#include <libmary/libmary_md5.h>
 
 #include <libmary/server_context.h>
 #include <libmary/server_thread_pool.h>
