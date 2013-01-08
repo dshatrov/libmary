5c8b34c56d4fff4cce7324837839debc396fda32
Dmitry Shatrov <shatrov@gmail.com>
2013-01-08 22:27:28 +0100

    test__exc update

--- tests/exc/test__exc.cpp
+++ tests/exc/test__exc.cpp
@@ -3,33 +3,112 @@
 
 using namespace M;
 
+class MyException : public Exception
+{
+public:
+    Ref<String> toString ()
+    {
+        if (cause)
+            return makeString ("MyException 0x", fmt_hex, (UintPtr) this, ": ", cause->toString()->mem());
+        else
+            return makeString ("MyException 0x", fmt_hex, (UintPtr) this);
+    }
+
+    MyException ()
+    {
+        errs->println (_func, "0x", fmt_hex, (UintPtr) this);
+    }
+
+    ~MyException ()
+    {
+        errs->println (_func, "0x", fmt_hex, (UintPtr) this);
+    }
+};
+
+static mt_throws Result test ()
+{
+    exc_throw <MyException> ();
+    return Result::Failure;
+}
+
+static void excPushScopeTest ()
+{
+    errs->println ("--- begin exc_pus_scope test ---");
+    {
+        exc_throw <MyException> ();
+        errs->println ("A exc: 0x", fmt_hex, (UintPtr) (Exception*) exc);
+
+        {
+            errs->println ("exc_push_scope");
+            exc_push_scope ();
+            errs->println ("exc: 0x", fmt_hex, (UintPtr) (Exception*) exc);
+
+            errs->println ("exc_throw");
+            exc_throw <MyException> ();
+
+            errs->println ("exc_push");
+            exc_push <MyException> ();
+
+            errs->println ("exc_none");
+            exc_none ();
+
+            errs->println ("exc_throw");
+            exc_throw <MyException> ();
+
+            errs->println ("exc_push");
+            exc_push <MyException> ();
+
+            errs->println ("exc: 0x", fmt_hex, (UintPtr) (Exception*) exc);
+            errs->println ("exception: ", exc->toString());
+
+            {
+                errs->println ("exc_push_scope");
+                exc_push_scope ();
+
+                errs->println ("exc_pop_scope");
+                exc_pop_scope ();
+            }
+
+            errs->println ("exc: 0x", fmt_hex, (UintPtr) (Exception*) exc);
+            errs->println ("exception: ", exc->toString());
+
+            errs->println ("exc_pop_scope");
+            exc_pop_scope ();
+        }
+
+        errs->println ("A exc: 0x", fmt_hex, (UintPtr) (Exception*) exc);
+
+        errs->println ("exc_none");
+        exc_none ();
+        errs->println ("exc: 0x", fmt_hex, (UintPtr) (Exception*) exc);
+    }
+    errs->println ("--- end exc_push_scope test ---");
+}
 
 int main (void)
 {
     libMaryInit ();
 
-    errs->print ("Hello, World!\n");
+    errs->println ("Hello, World!");
 
-    NativeFile file ("my_test_file", 0 /* open_flags */, File::AccessMode::WriteOnly);
-    if (exc)
-#if 0
-	debug_g (grp_all)
-	info_g (grp_all)
+    {
+        NativeFile file ("my_test_file", 0 /* open_flags */, File::AccessMode::WriteOnly);
+        if (exc)
+            logs->println ("Open: ", exc->toString());
 
-	debug ()
-	info ()
-	warn ()
-	error ()
-	fail ()
-#endif
-	logs->print ("Open: ").print (exc->toString ()->mem ()).print ("\n");
-//	debug ("Open: ", exc, "\n");
+        if (!file.println ("TEST OUTPUT"))
+            logs->println ("Print: ", exc->toString());
 
-    if (!file.print ("TEST OUTPUT\n"))
-	logs->print ("Print: ").print (exc->toString ()->mem ()).print ("\n");
+        if (!file.close ())
+            logs->println ("Close: ", exc->toString ());
+    }
 
-    if (!file.close ())
-	logs->print ("Close: ").print (exc->toString ()->mem ()).print ("\n");
+    if (!test ()) {
+        logs->println ("test: ", exc->toString());
+        exc_none ();
+    }
+
+    excPushScopeTest ();
 
     return 0;
 }
